e5f5f185281c979c285697bd1282a7bc26d0e19d
Aaron Pitarch <aapitarch@gmail.com>
2024-09-07 13:15:06 +0200

    Home Screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: () {
+              
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Welcome to Home Screen', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+            const SizedBox(height: 16),
+            const Text('Tu Coleccion', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      leading: const Icon(Icons.toys),
+                      title: Text('Funko Pop ${index + 1}'),
+                      subtitle: const Text('Descripcion breve'),
+                      onTap: () {
+                        
+                      },
+                    ),
+                  );
+                },
+              )
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
